6192a1c8b5d8581e609793d98991bd21610ead11
AlexanderSteffensen <asteff20@student.aau.dk>
2021-12-09 19:42:46 +0100

    first commit

---
+++ StregsystemMenuBarApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StregsystemMenuBarApp
+//
+//  Created by Alexander Steffensen on 09/12/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
