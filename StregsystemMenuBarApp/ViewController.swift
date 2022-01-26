3f1d7e93b0c3739c81f345302bd7fddb9beddc55
AlexanderSteffensen <asteff20@student.aau.dk>
2022-01-26 15:12:16 +0100

    changes made

--- StregsystemMenuBarApp/ViewController.swift
+++ StregsystemMenuBarApp/ViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import Cocoa
+import Foundation
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var product: NSPopUpButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +24,43 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func productChanged(_ sender: Any) {
+    }
+    
+    @IBAction func buyProduct(_ sender: Any) {
+    }
+    
+    
+    @IBAction func updateBalance(_ sender: Any) {
+        let dollars = executeCommand("python -u TheMindFrom99 -b")
+        let errorMessage = "Noget gik galt 403"
+        
+        if (dollars == errorMessage){
+        
+        }
+        else {
+            syste
+        }
+        
+        
+        
+    }
+    
+    func executeCommand(_ command: String) -> String{
+        let task = Process()
+        let pipe = Pipe()
+        
+        task.standardOutput = pipe
+        task.standardError = pipe
+        task.arguments = ["-c", command]
+        task.launchPath = "~/repositories/stregsystem-cli"
+        task.launch()
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: .utf8)!
+        
+        return output
+    }
+    
 }
 
---
+++ StregsystemMenuBarApp/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  StregsystemMenuBarApp
+//
+//  Created by Alexander Steffensen on 09/12/2021.
+//
+
+import Foundation
+
+let app = NSApplication.shared;
+let delegate = AppDelegate();
